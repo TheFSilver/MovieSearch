f4edf0d81fa9a6a370c6182a38621933e47387a7
TheFSilver <dasylva.f@gmail.com>
2018-08-10 00:46:43 +0200

    Programme fonctionnel sans images et sans css...

--- app/services/search_movie.rb
+++ app/services/search_movie.rb
@@ -8,23 +8,31 @@ class SearchMovie
     Tmdb::Api.key(ENV["DBMOVIE_KEY"])
 
     all_results = Tmdb::Search.movie(titre).results[number]
-      @id = all_results.id
-      movie_hash = Tmdb::Movie.detail(@id)
-      movies = []
-      movies << movie_hash["title"]
-      movies << movie_hash["release_date"]
-      crew_hash = Tmdb::Movie.crew(@id)
+    @id = all_results.id
+    sleep(1)
+    movie_hash = Tmdb::Movie.detail(@id)
+    movies = []
+    movies << movie_hash["title"]
+    movies << movie_hash["release_date"]
 
-      crew_hash.each do |an_array|
-        if an_array.select {|k,v| v.include?("Director") }
-          p an_array
-          #movies << the_array["name"]
-          #p the_array["name"]
-        end
+    crew_hash = Tmdb::Movie.crew(@id)
+    crew_hash.each do |an_array|
+      if an_array["job"] == "Director"
+        movies << an_array["name"]
+        sleep(1)
       end
-      #movies << crew_hash["name"]
-      #return crew_hash
-      return "Titre du film: #{movies[0]}, Date de sortie: #{movies[1]}, Réalisateur: #{movies[2]}}"
+    end
+    if movies[4] != nil
+      return "Titre du film: #{movies[0]} \nDate de sortie: #{movies[1]} \nRéalisateurs: #{movies[2]}, #{movies[3]} et #{movies[4]}"
+    elsif movies[3] != nil && movies[4] == nil
+      return "Titre du film: #{movies[0]} \nDate de sortie: #{movies[1]} \nRéalisateurs: #{movies[2]} et #{movies[3]}"
+    elsif movies[2] != nil && movies[3] == nil
+      return "Titre du film: #{movies[0]} \nDate de sortie: #{movies[1]} \nRéalisateur: #{movies[2]}"
+    elsif movies[2] == nil
+      return "Titre du film: #{movies[0]} \nDate de sortie: #{movies[1]} \nRéalisateur: information inconnue"
+    elsif movies[0] == nil
+      return nil
+    end
   end
 
   def perform(title, num)
